cdcb6ee37217f15ac3620c3438bbf1e6a6feb407
Kervendurdy Allaberdiyev <110550683+kervendurdy@users.noreply.github.com>
2025-01-13 11:31:34 +0300

    Update receive_dictionary_message.py

--- examples/receive_dictionary_message.py
+++ examples/receive_dictionary_message.py
@@ -28,7 +28,7 @@ loraSerial = serial.Serial('/dev/ttyS3') #(, baudrate=9600, parity=serial.PARITY
 
 lora = LoRaE220('900T22D', loraSerial, aux_pin='PA7', m0_pin='PA8', m1_pin='PA9')
 code = lora.begin()
-print("Initialization: {}", ResponseStatusCode.get_description(code))
+print(f"Initialization: {ResponseStatusCode.get_description(code)}")
 
 # Set the configuration to default values and print the updated configuration to the console
 # Not needed if already configured
@@ -42,7 +42,7 @@ configuration_to_set = Configuration('900T22D')
 configuration_to_set.TRANSMISSION_MODE.enableRSSI = RssiEnableByte.RSSI_ENABLED
 
 code, confSetted = lora.set_configuration(configuration_to_set)
-print("Set configuration: {}", ResponseStatusCode.get_description(code))
+print(f"Set configuration: {ResponseStatusCode.get_description(code)}")
 
 print("Waiting for messages...")
 while True:

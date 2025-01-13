d6c8d45762935bc0a03f02003c1a66c5cf8e8e10
Kervendurdy Allaberdiyev <110550683+kervendurdy@users.noreply.github.com>
2025-01-13 11:25:50 +0300

    Update send_transparent_dictionary_message.py

--- examples/send_transparent_dictionary_message.py
+++ examples/send_transparent_dictionary_message.py
@@ -2,10 +2,10 @@
 # Website: www.mischianti.org
 #
 # Description:
-# This script demonstrates how to use the E220 LoRa module with Raspberry Pi.
+# This script demonstrates how to use the E220 LoRa module with Orange Pi.
 # Sending dictionary
 #
-# Note: This code was written and tested using Raspberry Pi on an ESP32 board.
+# Note: This code was written and tested using Orange Pi on an ESP32 board.
 #       It works with other boards, but you may need to change the UART pins.
 
 import serial
@@ -15,14 +15,15 @@ from lora_e220_constants import RssiAmbientNoiseEnable, RssiEnableByte
 from lora_e220_operation_constant import ResponseStatusCode
 
 # Initialize the LoRaE220 module
-loraSerial = serial.Serial('/dev/serial0') #, baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
-lora = LoRaE220('400T22D', loraSerial, aux_pin=18, m0_pin=23, m1_pin=24)
+loraSerial = serial.Serial('/dev/ttyS3') #(, baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+
+lora = LoRaE220('900T22D', loraSerial, aux_pin='PA7', m0_pin='PA8', m1_pin='PA9')
 code = lora.begin()
 print("Initialization: {}", ResponseStatusCode.get_description(code))
 
 # Set the configuration to default values and print the updated configuration to the console
 # Not needed if already configured
-configuration_to_set = Configuration('400T22D')
+configuration_to_set = Configuration('900T22D')
 # To enable RSSI, you must also enable RSSI on receiver
 configuration_to_set.TRANSMISSION_MODE.enableRSSI = RssiEnableByte.RSSI_ENABLED
 code, confSetted = lora.set_configuration(configuration_to_set)
